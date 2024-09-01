8888f75bb8bcf285ba3a914880ff8a9c0a283bfe
Jvlauta <lautaro37pumm@hotmail.com>
2024-09-01 18:00:05 -0300

    cambio de carpeta 2

---
+++ Meto_y_prog/Actividad1/Ejercicio1/Ejercicio4/Pila.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lauta
+ * Date: 30/08/2024
+ * Time: 04:18 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ejercicio4
+{
+	public class Pila:IColeccionable
+	{
+		public Class1()
+		{
+		}
+	//metodos
+
+	//metodos Icollec
+
+	
+	public int Cuantos()
+	{
+		
+	}
+	
+	public int Minimo()
+	{
+		
+	}
+	
+	public List<IComparable<t>> Agregar(IComparable)
+	{
+		
+	}
+	
+	public bool Contiene(IComparable)
+	{
+		
+	}
+		
+		
+	}
+}
